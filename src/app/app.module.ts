b3a4c7a196a1fae158fc2127ea7d5afd8462771a
santosh <santosh.molleti00@gmail.com>
2023-08-27 01:22:16 +0530

    Basic structure of Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './body/home/home.component';
+import { LoginComponent } from './body/login/login.component';
+import { SignupComponent } from './body/signup/signup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
