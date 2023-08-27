3bc3f2d49f4e63c577c1e93a2fa90a2860886754
santosh <santosh.molleti00@gmail.com>
2023-08-27 12:44:59 +0530

    Routing implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './body/home/home.component';
+import { LoginComponent } from './body/login/login.component';
+import { SignupComponent } from './body/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'/signup',pathMatch:'full'},
+  {path:"home",component:HomeComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"login",component:LoginComponent},
+  {path:"**",redirectTo:'/signup',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
