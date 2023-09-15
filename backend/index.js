bbc2ac5a8be2b53d199e32072594b8238771dc65
santosh <santosh.molleti00@gmail.com>
2023-09-15 15:39:39 +0530

    add database connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,13 @@ const exp=require("express")
 const app=exp()
 const env=require("dotenv").config()
 const port=process.env.PORT
+const db=process.env.DATABASE
+const mongoose=require("mongoose")
+mongoose.connect(db).then(()=>{
+    console.log("Database is connected")
+}).catch((err)=>{
+    console.log("error",err)
+})
 app.listen(port,()=>{
     console.log(`Server is listening on ${port}`)
 })
